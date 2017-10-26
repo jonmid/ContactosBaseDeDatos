14fbcb8c3167a26c9e01ac787fdd10e1ff44fb2a
JonMid <jamideros@gmail.com>
2017-10-26 09:49:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jonmid/contactosbasededatos/ContactsActivity.java
@@ -0,0 +1,48 @@
+package com.example.jonmid.contactosbasededatos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.jonmid.contactosbasededatos.Adapters.ContactAdapter;
+import com.example.jonmid.contactosbasededatos.Helpers.SqliteHelper;
+import com.example.jonmid.contactosbasededatos.Models.Contact;
+import com.example.jonmid.contactosbasededatos.Views.RegisterContactActivity;
+import com.example.jonmid.contactosbasededatos.Views.SearchContactActivity;
+
+import java.util.List;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    RecyclerView recyclerViewContacts;
+    ContactAdapter contactAdapter;
+    List<Contact> contactList;
+    SqliteHelper sqliteHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        recyclerViewContacts = (RecyclerView) findViewById(R.id.id_rv_contacts);
+        sqliteHelper = new SqliteHelper(this, "db_contacts", null, 1);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerViewContacts.setLayoutManager(linearLayoutManager);
+    }
+
+    public void onClickShowWindowRegister(View view){
+        Intent intent = new Intent(this, RegisterContactActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickShowWindowSearch(View view){
+        Intent intent = new Intent(this, SearchContactActivity.class);
+        startActivity(intent);
+    }
+}
