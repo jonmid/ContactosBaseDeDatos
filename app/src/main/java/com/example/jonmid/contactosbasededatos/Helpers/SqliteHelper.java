fe7ba4c2ac3b26fd8a37b60b3cdd5b0e45bd7475
JonMid <10jonmid@gmail.com>
2017-11-18 09:29:10 -0500

    Crear registros al momento de instalar la aplicacion

--- app/src/main/java/com/example/jonmid/contactosbasededatos/Helpers/SqliteHelper.java
+++ app/src/main/java/com/example/jonmid/contactosbasededatos/Helpers/SqliteHelper.java
@@ -1,5 +1,6 @@
 package com.example.jonmid.contactosbasededatos.Helpers;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -20,6 +21,17 @@ public class SqliteHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(Constants.CREATE_TABLE_USERS);
         db.execSQL(Constants.CREATE_TABLE_COMMENTS);
+
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('aaaaa', '11111', 'aaaaa@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('bbbbb', '22222', 'bbbbb@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('ccccc', '33333', 'ccccc@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('ddddd', '44444', 'ddddd@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('eeeee', '55555', 'eeeee@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('fffff', '66666', 'fffff@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('ggggg', '77777', 'ggggg@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('hhhhh', '88888', 'hhhhh@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('iiiii', '99999', 'iiiii@gmail.com')");
+        db.execSQL("INSERT INTO users (name, phone, email) VALUES ('jjjjj', '10101', 'jjjjj@gmail.com')");
     }
 
     @Override
